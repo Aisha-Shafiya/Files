b5d7392348ad053c94750fdcec61cb8dde0d571e
Aisha-Shafiya <78399304+Aisha-Shafiya@users.noreply.github.com>
2024-03-05 12:12:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,9 @@
-const rangeInput = document.querySelectorAll('.range-input input');
-const priceInput = document.querySelectorAll('.price-input input');
-const progress = document.querySelector('.slider .progress');
-let priceGap = 1000;
-console.log(rangeInput);
-
-priceInput.forEach(input => {
-   input.addEventListener("input", (e)=> {
-    // Getting into two input values
-    let minVal = parseInt(priceInput[0].value),
-        maxVal = parseInt(priceInput[1].value);
-        if(maxVal -minVal < priceGap){
-        if(e.target.className === 'input-min')
-        {
-            rangeInput[0].value = maxVal;
-            progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-        }
-        else{
-            rangeInput[1].value = minVal;
-            progress.style.right = 100 -(maxVal / rangeInput[1].max) * 100 + "%";
-        }
-        }
-      
-    // console.log(minVal, maxVal);
-
-  
-    // console.log(percent);
-   }) 
-})
-
-
-
-rangeInput.forEach(input => {
-    input.addEventListener("input", (e)=> {
-     let minVal = parseInt(rangeInput[0].value),
-         maxVal = parseInt(rangeInput[1].value);
-         if(maxVal -minVal < priceGap){
-         if(e.target.className === 'range-min')
-         {
-             rangeInput[0].value = maxVal - priceGap;
-         }
-         else{
-             rangeInput[1].value = minVal + priceGap;
-         }
-         }
-         else{
-             priceInput[0].value = minVal;
-             priceInput[1].value = maxVal
-             progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-             progress.style.right = 100 -(maxVal / rangeInput[1].max) * 100 + "%";
-         }
-     // console.log(minVal, maxVal);
- 
-   
-     // console.log(percent);
-    }) 
- })
+function myFunction(){
+    var x = document.getElementById('myTopnav');
+    if ( x.className === 'topnav'){
+        x.className += ' responsive';
+    }
+    else{
+        x.className = 'topnav';
+    }
+}
